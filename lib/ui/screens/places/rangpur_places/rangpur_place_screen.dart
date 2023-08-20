89be9889043282652108713356a99de53f1c322f
Tanvir <tanvirahmed199823@gmail.com>
2023-08-20 18:36:05 +0600

    rename folder

---
+++ lib/ui/screens/places/rangpur_places/rangpur_place_screen.dart
@@ -0,0 +1,279 @@
+import 'package:flutter/material.dart';
+import 'package:travel_guide/ui/screens/utils/asset_utils.dart';
+import 'package:travel_guide/ui/widgets/app_bar_widget.dart';
+
+class RangpurPlacesScreen extends StatelessWidget {
+  const RangpurPlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const OpAppBar(),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24,right: 24,top: 24),
+                    child: GestureDetector(
+                      onTap: (){
+                        //Navigator.push(context, MaterialPageRoute(builder: (context)=>const DhakaDetailsScreen()));
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(bottom: 8.00),
+                            child: Text(
+                              'Dhaka >',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.00),
+                            child: Image.asset(AssetsUtils.dhakaPng),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24,right: 24,top: 24),
+                    child: GestureDetector(
+                      onTap: (){
+                        //Navigator.push(context, MaterialPageRoute(builder: (context)=>SplashScreen()));
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(bottom: 8.00),
+                            child: Text(
+                              'Dhaka >',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.00),
+                            child: Image.asset(AssetsUtils.dhakaPng),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24,right: 24,top: 24),
+                    child: GestureDetector(
+                      onTap: (){
+                        //Navigator.push(context, MaterialPageRoute(builder: (context)=>SplashScreen()));
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(bottom: 8.00),
+                            child: Text(
+                              'Dhaka >',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.00),
+                            child: Image.asset(AssetsUtils.dhakaPng),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24,right: 24,top: 24),
+                    child: GestureDetector(
+                      onTap: (){
+                        //Navigator.push(context, MaterialPageRoute(builder: (context)=>SplashScreen()));
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(bottom: 8.00),
+                            child: Text(
+                              'Dhaka >',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.00),
+                            child: Image.asset(AssetsUtils.dhakaPng),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24,right: 24,top: 24),
+                    child: GestureDetector(
+                      onTap: (){
+                        //Navigator.push(context, MaterialPageRoute(builder: (context)=>SplashScreen()));
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(bottom: 8.00),
+                            child: Text(
+                              'Dhaka >',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.00),
+                            child: Image.asset(AssetsUtils.dhakaPng),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24,right: 24,top: 24),
+                    child: GestureDetector(
+                      onTap: (){
+                        //Navigator.push(context, MaterialPageRoute(builder: (context)=>SplashScreen()));
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(bottom: 8.00),
+                            child: Text(
+                              'Dhaka >',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.00),
+                            child: Image.asset(AssetsUtils.dhakaPng),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24,right: 24,top: 24),
+                    child: GestureDetector(
+                      onTap: (){
+                        //Navigator.push(context, MaterialPageRoute(builder: (context)=>SplashScreen()));
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(bottom: 8.00),
+                            child: Text(
+                              'Dhaka >',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.00),
+                            child: Image.asset(AssetsUtils.dhakaPng),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24,right: 24,top: 24),
+                    child: GestureDetector(
+                      onTap: (){
+                        //Navigator.push(context, MaterialPageRoute(builder: (context)=>SplashScreen()));
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(bottom: 8.00),
+                            child: Text(
+                              'Dhaka >',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.00),
+                            child: Image.asset(AssetsUtils.dhakaPng),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24,right: 24,top: 24),
+                    child: GestureDetector(
+                      onTap: (){
+                        //Navigator.push(context, MaterialPageRoute(builder: (context)=>SplashScreen()));
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(bottom: 8.00),
+                            child: Text(
+                              'Dhaka >',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.00),
+                            child: Image.asset(AssetsUtils.dhakaPng),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
